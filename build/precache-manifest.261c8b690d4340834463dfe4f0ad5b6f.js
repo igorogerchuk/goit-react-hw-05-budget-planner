fe8233cd03a227d676805cbc1eb79ebee9d80a44
Igor <igor.ogerchuk@gmail.com>
2020-01-08 00:11:36 +0200

    added build folder

---
+++ build/precache-manifest.261c8b690d4340834463dfe4f0ad5b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19c484e0c533b9ca452dc49bc04d1e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da6562440c77a29e175c",
+    "url": "/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "4b3a15e7985340ded806",
+    "url": "/static/js/2.0fd47f36.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.0fd47f36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da6562440c77a29e175c",
+    "url": "/static/js/main.bab47c37.chunk.js"
+  },
+  {
+    "revision": "af30c3dbf156fd1cf104",
+    "url": "/static/js/runtime-main.d94da19e.js"
+  }
+]);
