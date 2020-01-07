ebcc3dc507f57dda14f94313a5fa7e56587f8fa6
Igor <igor.ogerchuk@gmail.com>
2020-01-08 00:33:04 +0200

    Updates

---
+++ precache-manifest.d89e05b570f215d0ab2823c571fd071a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edfbd20d5635b22a78dc2da607855038",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "da6562440c77a29e175c",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "a4991eb4a6a8216de0fc",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.9868dd2a.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.9868dd2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da6562440c77a29e175c",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.bab47c37.chunk.js"
+  },
+  {
+    "revision": "b74359a5e5e42ec3281f",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.7176f55a.js"
+  }
+]);
